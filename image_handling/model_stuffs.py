d4d695a214136195f6e401e7a101457a09bc010c
unta1337 <firstapple42@gamil.com>
2023-01-05 15:00:27 +0900

    Temp Commit

--- image_handling/model_stuffs.py
+++ image_handling/model_stuffs.py
@@ -1,5 +1,7 @@
 import os
 
+from math import ceil
+
 from config import *
 
 def train_images(dataset_name: str):
@@ -10,4 +12,15 @@ def train_images(dataset_name: str):
         dataset_name: 학습을 수행할 데이터셋 이름.
     """
 
-    os.system(f'python {YOLO_PREFIX}train.py --data {dataset_name}.yaml --epochs 500 --weights yolov5n.pt --batch-size 128 --name {dataset_name}')
+    # epoch, batch 수정
+    # epoch: 500, batch: 128
+    # epoch: 5, batch: 1000
+    # epoch: 10, batch : 64
+
+    path = DATASET_PREFIX + dataset_name
+    img_names = os.listdir(path)
+
+    num_epochs = 10 #5 #4
+    num_batch = 64 #32
+
+    os.system(f'python {YOLO_PREFIX}train.py --data {dataset_name}.yaml --epochs {num_epochs} --weights yolov5n.pt --batch-size {num_batch} --name {dataset_name}')
