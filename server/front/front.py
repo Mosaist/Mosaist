e54097de12cebc9d65bd03954c30bdb459c24bcf
unta1337 <firstapple42@gmail.com>
2023-02-07 13:36:34 +0900

    리소스에 대한 처리 추가

--- server/front/front.py
+++ server/front/front.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 config = json.load(open(f'../../config.json'))
 
@@ -10,8 +10,12 @@ def root():
     return render_template('index.html', config=config)
 
 @app.route('/<url>')
-def map(url):
+def template(url):
     return render_template(url, config=config)
+
+@app.route('/resource/<url>')
+def resource(url):
+    return send_file(f'resources/{url}')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=config['server']['front']['port'])
