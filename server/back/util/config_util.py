d2932f203bca3b2e06545c6114311f5279ce5795
unta1337 <firstapple42@gmail.com>
2023-02-26 01:26:05 +0900

    config_util에 json_util 병합

--- server/back/util/config_util.py
+++ server/back/util/config_util.py
@@ -1,6 +1,12 @@
 import os
 import json
 
-import util.json_util as json_util
+class DotDict(dict):
+    def __getattr__(*args):
+        val = dict.get(*args)
+        return DotDict(val) if type(val) is dict else val
 
-CONFIG = json_util.DotDict(json.load(open(f'{os.path.dirname(__file__)}/../../../config.json')))
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+CONFIG = DotDict(json.load(open(f'{os.path.dirname(__file__)}/../../../config.json')))
