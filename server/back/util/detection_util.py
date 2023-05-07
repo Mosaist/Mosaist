a753c4dd6031f7826ba425ff9cd9dea61cc3b934
unta1337 <firstapple42@gmail.com>
2023-05-08 00:37:10 +0900

    iou 로직 변경

--- server/back/util/detection_util.py
+++ server/back/util/detection_util.py
@@ -1,37 +1,27 @@
-def get_area(b):
-    w = b['xmax'] - b['xmin']
-    h = b['ymax'] - b['ymin']
-
-    return w * h
-
-def get_inter(p, q):
-    if p['xmin'] > q['xmax']:
-        return False
-    elif p['xmax'] < q['xmin']:
-        return False
-    if p['ymin'] > q['ymax']:
-        return False
-    elif p['ymax'] < q['ymax']:
-        return False
-
-    return {
-        'xmin': max(p['xmin'], q['xmin']),
-        'xmax': min(p['xmax'], q['xmax']),
-        'ymin': max(p['ymin'], q['ymin']),
-        'ymax': min(p['ymax'], q['ymax'])
-    }
-
-def get_intersection_of_union(p, q):
-    area_p = get_area(p)
-    area_q = get_area(q)
+def get_intersection_over_union(p, q):
+    x1_1, y1_1, x2_1, y2_1 = (p['xmin'], p['ymin'], p['xmax'], p['ymax'])
+    x1_2, y1_2, x2_2, y2_2 = (q['xmin'], q['ymin'], q['xmax'], q['ymax'])
     
-    inter = get_inter(p, q)
+    # 겹치는 영역 좌상단, 우하단 좌표 계산
+    x_left = max(x1_1, x1_2)
+    y_top = max(y1_1, y1_2)
+    x_right = min(x2_1, x2_2)
+    y_bottom = min(y2_1, y2_2)
 
-    if not inter:
-        return 0
+    # 겹치는 영역의 너비와 높이 계산
+    intersection_area = max(0, x_right - x_left + 1) * max(0, y_bottom - y_top + 1)
 
-    area_inter = get_area(inter)
-    return area_inter / (area_p + area_q - area_inter)
+    # 전체 영역의 너비와 높이 계산
+    box1_area = (x2_1 - x1_1 + 1) * (y2_1 - y1_1 + 1)
+    box2_area = (x2_2 - x1_2 + 1) * (y2_2 - y1_2 + 1)
+
+    # 두 영역의 합집합 면적 계산
+    union_area = box1_area + box2_area - intersection_area
+
+    # IoU 계산
+    iou = intersection_area / union_area
+
+    return iou
 
 def apply_detections_nms(detections, iou_criteria=0.5):
     nms_applied = []
@@ -43,7 +33,7 @@ def apply_detections_nms(detections, iou_criteria=0.5):
         for i, det in enumerate(detection):
             keep = True
             for higher_conf_det in detection[i + 1:]:
-                if get_intersection_of_union(det, higher_conf_det) > iou_criteria:
+                if get_intersection_over_union(det, higher_conf_det) > iou_criteria:
                     keep = False
                     break
 
