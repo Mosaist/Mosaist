53a43bb1ae15e531a86a259586cf2c8a7bb5defb
unta1337 <firstapple42@gmail.com>
2023-05-20 15:20:56 +0900

    소켓의 socket_image_mosaic가 optical flow를 사용하도록 변경

--- server/back/socket_api.py
+++ server/back/socket_api.py
@@ -1,8 +1,10 @@
 import datetime
 import ssl
 import asyncio
-
 import websockets
+
+import cv2
+import numpy as np
 
 import util.image_util as image_util
 
@@ -10,6 +12,7 @@ from mosaic.recognizer import Recognizer
 from util.config_util import CONFIG
 
 rec = Recognizer()
+temp_storage = { }
 
 async def socket_root(websocket):
     async for message in websocket:
@@ -20,9 +23,9 @@ async def socket_root(websocket):
 
         try:
             if method == 'image-rect':
-                response = socket_image_rect(content)
+                response = socket_image_rect(content, websocket.id)
             elif method == 'image-mosaic':
-                response = socket_image_mosaic(content)
+                response = socket_image_mosaic(content, websocket.id)
             else:
                 response = 'Method not found'
         except:
@@ -30,15 +33,38 @@ async def socket_root(websocket):
 
         await websocket.send(response)
 
-def socket_image_rect(content):
+def socket_image_rect(content, id):
     image = image_util.from_socket(content)
     image = rec.rect_images([image], do_sieve=False)[0][0]
 
     return image_util.to_png_byte(image)
 
-def socket_image_mosaic(content):
+def socket_image_mosaic(content, id):
+    if id not in temp_storage:
+        temp_storage[id] = { }
+        temp_storage[id]['index'] = 0
+        temp_storage[id]['prev_image'] = None
+        temp_storage[id]['prev_corners'] = None
+        temp_storage[id]['prev_windows'] = None
+
     image = image_util.from_socket(content)
-    image = rec.rect_images([image], do_sieve=False)[0][0]
+    image_temp = image.copy()
+
+    if temp_storage[id]['index'] % 10 == 0:
+        image, detections = rec.rect_images([image], do_sieve=False)
+        image = image[0]
+        temp_storage[id]['prev_corners'] = [[det['xmin'], det['ymin']] for det in detections[0]]
+        temp_storage[id]['prev_windows'] = [[det['xmax'] - det['xmin'], det['ymax'] - det['ymin']] for det in detections[0]]
+    elif len(temp_storage[id]['prev_corners']) > 0:
+        current_corners, _, _ = cv2.calcOpticalFlowPyrLK(temp_storage[id]['prev_image'], image, np.array(temp_storage[id]['prev_corners'], dtype=np.float32), None)
+        temp_storage[id]['prev_corners'] = current_corners
+
+        for corner, window in zip(current_corners, temp_storage[id]['prev_windows']):
+            corner = tuple(map(int, corner))
+            cv2.rectangle(image, corner, (corner[0] + window[0], corner[1] + window[1]), (255, 0, 0), 3)
+
+    temp_storage[id]['prev_image'] = image_temp
+    temp_storage[id]['index'] += 1
 
     return image_util.to_png_byte(image)
 
